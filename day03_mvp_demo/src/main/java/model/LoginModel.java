f509301cbddf96a1e38b88c7f996b5f20b5085f1
Androidlvfan <lvfan.12138@qq.com>
2018-12-27 15:23:28 +0800

    master

--- day03_mvp_demo/src/main/java/model/LoginModel.java
+++ day03_mvp_demo/src/main/java/model/LoginModel.java
@@ -8,6 +8,6 @@ public class LoginModel implements ILoginModel {
     //继承Model接口，实现接口中的所有方法
     @Override
     public void login(HashMap<String, String> params, RequestCallback requestCallback) {
-//wwwwwwwww2wwww
+
     }
 }
